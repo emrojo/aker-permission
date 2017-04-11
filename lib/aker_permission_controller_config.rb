5de9fecd74d18fec2281519491c6d8b7aa245468
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-04-11 13:26:04 +0100

    Added PermissionConfig module

---
+++ lib/aker_permission_controller_config.rb
@@ -0,0 +1,16 @@
+module AkerPermissionControllerConfig
+  def self.included(base)
+    base.instance_eval do
+      check_authorization
+
+      rescue_from CanCan::AccessDenied do |exception|
+        respond_to do |format|
+          format.api_json { head :forbidden, content_type: 'application/vnd.api+json' }
+          format.json { head :forbidden, content_type: 'text/html' }
+          format.html { redirect_to root_path, alert: exception.message }
+          format.js   { head :forbidden, content_type: 'text/html' }
+        end
+      end
+    end
+  end
+end
--- lib/aker_permission_gem.rb
+++ lib/aker_permission_gem.rb
@@ -2,6 +2,7 @@ require "aker_permission_gem/version"
 
 autoload :Ability, 'ability'
 autoload :Accessible,      'concerns/accessible'
+autoload :AkerPermissionControllerConfig, 'aker_permission_controller_config'
 
 module AkerPermissionGem
   autoload :Ability,         'ability'
