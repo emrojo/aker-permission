184fbc371bddb467859e71ae3a0f06ba483eab9b
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-04-19 12:10:39 +0100

    If json api is not included, ignore the format.api_json

--- lib/aker_permission_controller_config.rb
+++ lib/aker_permission_controller_config.rb
@@ -6,7 +6,7 @@ module AkerPermissionControllerConfig
 
       rescue_from CanCan::AccessDenied do |exception|
         respond_to do |format|
-          format.api_json { head :forbidden, content_type: 'application/vnd.api+json' }
+          format.api_json { head :forbidden, content_type: 'application/vnd.api+json' } if format.respond_to? :api_json          
           format.json { head :forbidden, content_type: 'text/html' }
           format.html { redirect_to root_path, alert: exception.message }
           format.js   { head :forbidden, content_type: 'text/html' }
