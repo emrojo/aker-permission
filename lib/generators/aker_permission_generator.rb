5723071971a0b4319302ea8f8733cbcbde1a7fb6
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-04-19 09:51:57 +0100

    Migration in a file, instead of generate it automatically

--- lib/generators/aker_permission_generator.rb
+++ lib/generators/aker_permission_generator.rb
@@ -1,5 +1,19 @@
 class AkerPermissionGenerator < Rails::Generators::Base
-  def create_permission_migration
-    generate "migration CreatePermissions permitted:string:index r:boolean w:boolean x:boolean accessible:references{polymorphic}"
+   include Rails::Generators::Migration
+
+  source_root File.expand_path('../templates', __FILE__)
+
+  def self.next_migration_number(dir)
+    Time.now.strftime("%Y%m%d%H%M%S")
   end
+
+  def accessible_id_type
+    return 'int' unless Rails.configuration.respond_to? :accessible_id_type
+    Rails.configuration.accessible_id_type
+  end
+
+  def create_permission_migration
+    migration_template 'migration_template.rb', 'db/migrate/create_permissions.rb'
+  end
+
 end
