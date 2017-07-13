e5a6a727492a9f9e2d09fd7b86abafa8360fce45
Eduardo Martin Rojo <emr@mib107770i.internal.sanger.ac.uk>
2017-07-13 15:03:04 +0100

    Include the permissions when accessing the instance while trying to authorize the user

--- lib/aker_permission_client_config.rb
+++ lib/aker_permission_client_config.rb
@@ -1,5 +1,5 @@
 module AkerPermissionClientConfig
-  def check_permissions?(role, user_email)
+  def has_permission?(user_email, role)
   	mapping = {read: :r, write: :w, execute: :x}  	
   	permissions.any? do |permission|
   	  (permission.permitted == user_email) && (permission.send(mapping[role]))
@@ -10,12 +10,20 @@ module AkerPermissionClientConfig
   	base.instance_eval do
 	  def self.authorize!(role, resource, user_email)
 	  	raise CanCan::AccessDenied.new("Not authorized!", role, resource) unless user_email
-		if resource.kind_of? String
-		  instance = where(id: resource).includes(:permissions).first
-		else
-		  instance = resource
-		end
-		unless instance.check_permissions?(role, user_email)
+
+	  	# We need to have a resource that has the permissions included. If the paramter does not have
+	  	# it, we obtain it again with the inclusion of the permissions
+	  	if resource.respond_to?(:permissions)
+	  	  instance = resource
+	  	else
+	  	  # If the parameter is an id:
+	  	  if resource.kind_of? String
+	  	  	instance = where(id: resource).includes(:permissions).first
+	  	  else
+	  	  	instance = where(id: resource.id).includes(:permissions).first
+	  	  end
+	  	end
+		unless instance.has_permissions?(user_email, role)
 		  raise CanCan::AccessDenied.new("Not authorised to perform #{role} on #{instance.class.to_s} #{instance.id}", 
 		  	role, instance)
 		end
