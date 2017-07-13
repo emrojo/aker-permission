e8dd386194d29549de23b27724a5fadcbba26c7b
Eduardo Martin Rojo <emr@mib107770i.internal.sanger.ac.uk>
2017-07-13 15:55:18 +0100

    Added another test

--- spec/aker_permission_client_config_spec.rb
+++ spec/aker_permission_client_config_spec.rb
@@ -31,6 +31,15 @@ RSpec.describe 'AkerPermissionClientConfig' do
           allow(@response).to receive(:permissions).and_return(@permissions)
 	  	  expect{MyClient.authorize!(@role, @resource, @user)}.to_not raise_error
 	  	end
+	  	it 'returns true if the user has at least one of the permissions specified set to true' do
+	  	  @permissions = [
+	  	  	double('Permission', permitted: 'user2', x: false),
+	  	  	double('Permission', permitted: 'user3', x: false),
+	  	  	double('Permission', permitted: @user, x: true)
+	  	  ]
+          allow(@response).to receive(:permissions).and_return(@permissions)
+	  	  expect{MyClient.authorize!(@role, @resource, @user)}.to_not raise_error
+	  	end	  	
 	  	it 'raises an exception if it has the permission specified set to false' do
 	  	  @permissions = [double('Permission', permitted: @user, x: false)]
           allow(@response).to receive(:permissions).and_return(@permissions)
