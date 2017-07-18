fa0893964938d17d86a23a5d263aec0c207f7093
Harriet Craven <hc6@sanger.ac.uk>
2017-07-18 14:42:58 +0100

    Updated authorize! class method in AkerPermissionClientConfig

--- lib/aker_permission_client_config.rb
+++ lib/aker_permission_client_config.rb
@@ -1,43 +1,17 @@
 module AkerPermissionClientConfig
-  def has_permission?(user_email, role)
+  def has_permission?(username_and_groups, role)
     permissions.any? do |permission|
-      permission.permitted == user_email && permission.permission_type.to_s==role.to_s
+      username_and_groups.include?(permission.permitted) && permission.permission_type.to_s==role.to_s
     end
   end
 
   def self.included(base)
 
     base.instance_eval do |klass|
-
-      # name = 'Permission'
-      # klass = Object.const_set name, Class.new(JsonApiClient::Resource) do 
-      #   belongs_to base
-      # end
-
-      # klass.instance_eval do
-      #   define_method :permission_type do
-      #     attributes["permission-type"]
-      #   end
-      # end
-
-      # has_many :permissions
-
-
-      def self.authorize!(role, resource, user_email)
-        # We need to have a resource that has the permissions included. If the paramter does not have
-        # it, we obtain it again with the inclusion of the permissions
-        if resource.respond_to?(:permissions)
-          instance = resource
-        else
-          # If the parameter is an id:
-          if resource.kind_of? String
-            instance = where(id: resource).includes(:permissions).first
-          else
-            instance = where(id: resource.id).includes(:permissions).first
-          end
-        end
-        unless instance.has_permission?(user_email, role)
-          raise AkerPermissionGem::NotAuthorized.new("Not authorised to perform #{role} on #{instance.class.to_s} #{instance.id}")
+      def self.authorize!(role, resource_id, username_and_groups)
+        instance = where(id: resource_id).includes(:permissions).first
+        unless instance.has_permission?(username_and_groups, role)
+          raise AkerPermissionGem::NotAuthorized.new("Not authorised to perform #{role} on #{self} #{resource_id}")
         end
       end
     end
