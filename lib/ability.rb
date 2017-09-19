39dead14c5ad4da60271dc2408a3208333b89407
David Robinson <dr6@sanger.ac.uk>
2017-09-19 13:25:54 +0100

    Support read permission even if there is no user

--- lib/ability.rb
+++ lib/ability.rb
@@ -6,7 +6,9 @@ class Ability
 
   def initialize(user)
     can do |permission_type, subject_class, subject|
-      if !subject
+      if !user && !subject
+        permission_type==:read
+      elsif !subject
         [:create, :read].include?(permission_type)
       else
         permitted?(subject, user, permission_type)
@@ -15,7 +17,7 @@ class Ability
   end
 
   def permitted?(accessible, user, permission_type)
-    return false if user.nil?
+    return (permission_type==:read) if user.nil?
     permission_type==:create || accessible&.permitted?(user.email, permission_type) || accessible&.permitted?(user.groups, permission_type)
   end
 end
