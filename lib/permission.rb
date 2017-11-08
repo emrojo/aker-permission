5b2092f756e58bff13420284aaa50ba4a7db1771
David Robinson <dr6@sanger.ac.uk>
2017-11-08 16:37:58 +0000

    sanitise 'permitted' (which is an email address or a group name)
    Sanitise by converting to lower case and stripping.

--- lib/permission.rb
+++ lib/permission.rb
@@ -1,5 +1,17 @@
 module AkerPermissionGem
   class Permission < ApplicationRecord
     belongs_to :accessible, polymorphic: true
+
+    before_save :sanitise_permitted
+    before_validation :sanitise_permitted
+
+    def sanitise_permitted
+      if permitted
+        sanitised = permitted.strip.downcase
+        if sanitised != permitted
+          self.permitted = sanitised
+        end
+      end
+    end
   end
 end
